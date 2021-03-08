4745dcfc8a46c8ed3312e7b86e7bda4a4f328a5d
jakub.cichorski@hotmail.com <jakub.cichorski@hotmail.com>
2021-03-08 17:22:05 +0100

    cleanup, added initial structure

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { styles } from './styles/App';
+import ChatList from './components/ChatList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ChatList />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/ChatList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import ChatRoom from './ChatRoom';
+
+export default ChatList = () => {
+    return (
+        <View>
+            <ChatRoom />
+        </View>
+    )
+}
